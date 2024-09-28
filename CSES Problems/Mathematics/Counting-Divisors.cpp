4f9f8f7b15c4a3ea3292ea4163e706220d062ff0
Md Khaled Bin Joha <joh60483@gmail.com>
2024-09-28 13:03:37 +0600

    Math Divisors

---
+++ CSES Problems/Mathematics/Counting-Divisors.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+const int maxX = 1e6;
+
+int N, x, d[maxX+1];
+
+void init(){
+    for(int i = 1; i <= maxX; i++)
+        for(int j = i; j <= maxX; j += i)
+            d[j]++;
+}
+
+int main(){
+    cin >> N;
+
+    init();
+    for(int i = 0; i < N; i++){
+        cin >> x;
+        cout << d[x] << endl;
+    }
+    return 0;
+}
